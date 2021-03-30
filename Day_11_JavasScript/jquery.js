a864cc3c3635dce2830683c852fb2f4e0435406e
Dinesh Prajapat <45159995+Dprajapat765@users.noreply.github.com>
2021-03-30 19:58:26 +0530

    Day_11

---
+++ Day_11_JavasScript/jquery.js
@@ -0,0 +1,10 @@
+// Javascript comment for single line
+/* Javascript comment for multiple lines */
+
+// defining variables
+var input1 = 15;
+var input2 = 6;
+var add = input1 + input2;
+
+// statement
+alert(input2+input1);
